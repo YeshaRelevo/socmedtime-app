2a06de25fb23da3cdc8efa2f7cb2450ff7b7baf0
YeshaRelevo <princesstumbaga2@gmail.com>
2025-06-19 10:24:11 +0800

    day2, 6/19/25

--- lib/main.dart
+++ lib/main.dart
@@ -3,44 +3,98 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.purple[70],
       appBar: AppBar(
-        title: Text('Socmedtime App'),
-        backgroundColor: Colors.purpleAccent[700],
-      ),
-      backgroundColor: Colors.purple[100],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Hi',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              'Hello',
-              style: TextStyle(
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-                color: Colors.deepPurple,
-              ),
-            ),
-            Text(
-              'Goodbye',
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.normal,
-                color: Colors.black,
-              ),
+        title: Text('Socmedtime App', style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+          color: Colors.white,
+          shadows: [
+            Shadow(blurRadius: 10.0,
+            color: Colors.black,
+            offset: Offset(2.0,2.0),
             ),
           ],
-        ),
+        ),),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Name:', style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text('Princes Yesha', style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Age:', style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text('21 Years Old', style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Gender Row with fromLTRB margin
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('Gender:', style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text('Female', style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     ),
   ));
 }
-
 
